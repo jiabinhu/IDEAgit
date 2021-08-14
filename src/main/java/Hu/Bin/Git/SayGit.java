5e4a15aeabfab12ba7040990cd713c4ad7c4496b
jiabinhu <1115806174@qq.com>
2021-08-14 21:44:38 +0800

    添加测试文件

---
+++ src/main/java/Hu/Bin/Git/SayGit.java
@@ -0,0 +1,12 @@
+package Hu.Bin.Git;
+
+/**
+ * @author shkstart
+ * @create 08-14-21:39
+ */
+public class SayGit {
+    public String saygit(String name){
+        return "Hello"+name;
+    }
+
+}
